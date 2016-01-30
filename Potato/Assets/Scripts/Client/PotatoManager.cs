d9f1f7271112da7a8a68a4e7e89182bc0fb4483f
unknown <1103410@MC10795.uad.ac.uk>
2016-01-30 22:50:13 +0000

    updated client

---
+++ Potato/Assets/Scripts/Client/PotatoManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PotatoManager : MonoBehaviour {
+
+    public GameObject potato;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Potato/Assets/Scripts/Client/PotatoPreparation.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PotatoPreparation : MonoBehaviour {
+
+    public GameObject m_UnpeeledPotato;
+    public GameObject m_PeeledPotato;
+    public GameObject m_Light;
+
+    public SocketComm m_Socket;
+
+    private bool m_ReadyToPeel;
+    private bool m_ReadyToSend;
+	// Use this for initialization
+	void Start () {
+
+        m_ReadyToPeel = false;
+        m_ReadyToSend = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        Touch[] touches = Input.touches;
+
+        if (touches.Length != 0)
+        {
+            if (touches[0].phase == TouchPhase.Moved)
+            {
+                if (m_UnpeeledPotato)
+                {
+                    m_ReadyToPeel = true;
+                }
+                else if (m_PeeledPotato)
+                {
+                    m_ReadyToSend = true;
+                }
+            }
+             
+            if(touches[0].phase == TouchPhase.Ended || touches[0].phase == TouchPhase.Canceled)
+            {
+                 if (m_UnpeeledPotato && m_ReadyToPeel)
+                 {
+                    Destroy(m_UnpeeledPotato);
+                    //SendPotato
+                    //m_Socket.RequestPotatoCreation();
+                 }
+
+                 if (m_PeeledPotato && m_ReadyToSend)
+                 {
+                     Destroy(m_PeeledPotato);
+                     //SendPotato
+                     m_Socket.RequestPotatoCreation();
+                     Destroy(gameObject);
+                 }
+            }
+
+        }
+
+	}
+}

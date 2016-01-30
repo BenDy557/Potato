b3fa7fafaae0cb6aef3271391f947a6a18e82f97
Nelson <1505202@abertay.ac.uk>
2016-01-30 14:03:59 +0000

    Added Socket System, Client Master Server Relation, Made Server Scene work

---
+++ Potato/Assets/Scripts/ServerCameraManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public class ServerCameraManager : MonoBehaviour
+{
+    [SerializeField] private SocketComm serverSocket = null;
+    [SerializeField] private Transform leftMargin = null;
+    [SerializeField] private Transform rightMargin = null;
+
+    private Camera camera = null;
+    private Transform cameraTransform = null;
+
+    private Vector3 lerpPosition;
+    private float lerpViewportSize;
+
+    private Transform myTransform;
+
+    private void Start()
+    {
+        camera = Camera.main;
+        cameraTransform = camera.GetComponent<Transform>();
+
+        myTransform = GetComponent<Transform>();
+
+        lerpViewportSize = camera.orthographicSize;
+        lerpPosition = cameraTransform.position;
+    }
+
+    private void Update()
+    {
+        cameraTransform.position = Vector3.Lerp(cameraTransform.position, lerpPosition, Time.deltaTime);
+        camera.orthographicSize = FloatLerp(camera.orthographicSize, lerpViewportSize, Time.deltaTime);
+    }
+
+    private float FloatLerp(float from, float to, float time)
+    {
+        return (from + (to - from) * time);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        UpdateCameraViewport();
+        UpdateCameraLimits();
+    }
+
+    private void UpdateCameraViewport()
+    {
+        lerpViewportSize *= 2;
+    }
+    private void UpdateCameraLimits()
+    {
+        // Save Info Before Modifying
+        Vector3 cameraPos = cameraTransform.position;
+        float size = camera.orthographicSize;
+
+        cameraTransform.position = lerpPosition;
+        camera.orthographicSize = lerpViewportSize;
+
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(new Vector3(0,0,0));
+        if (Physics.Raycast(ray, out hit, 1000000))
+        {
+            float colliderOffset = 0.5f;
+
+            serverSocket.UpdateSpawnLimits(hit.point.x, -hit.point.x);
+            lerpPosition.y += -hit.point.y;
+            myTransform.position += new Vector3(0, -hit.point.y * 2 + colliderOffset * 2, 0);
+
+            leftMargin.position = new Vector3(hit.point.x - colliderOffset, 500000, 0);
+            rightMargin.position = new Vector3(-hit.point.x + colliderOffset , 500000, 0);
+        }
+
+        cameraTransform.position = cameraPos;
+        camera.orthographicSize = size;
+    }
+}
---
+++ Potato/Assets/Scripts/SocketComm.cs
@@ -0,0 +1,161 @@
+﻿using UnityEngine;
+
+using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Collections.Generic;
+
+public enum ESocketBehaviour { None, Server, Client }
+
+public class SocketComm : MonoBehaviour
+{
+    [Header("Socket Connection")]
+    [SerializeField] private ESocketBehaviour socketBehaviour = ESocketBehaviour.None;
+
+    [SerializeField] private string IP = "";
+    [SerializeField] private int port = 0;
+
+    private Socket mySocket = null;
+    private byte[] buffer;
+
+    [Header("Server Variables")] // Server Variables
+    public GameObject potatoPrefab;
+    private List<Socket> clients = null; // Only Initialize It If It's The Server
+    private int potatoesToSpawn = 0;
+    private Vector2 limits;
+
+    /// <summary>
+    /// Socket Initialization Place Any Other Code Before the Socket Initialization
+    /// </summary>
+    private void Start()
+    {
+        if (socketBehaviour == ESocketBehaviour.Server)
+        {
+            clients = new List<Socket>();
+
+            buffer = new byte[1024];
+
+            mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint parsedIP = new IPEndPoint(IPAddress.Parse(IP), port);
+
+            mySocket.Bind(parsedIP);
+            mySocket.Listen(100);
+            mySocket.BeginAccept( new AsyncCallback(AcceptCallback), null );
+
+            return;
+        }
+
+        if (socketBehaviour == ESocketBehaviour.Client)
+        {
+            buffer = new byte[1];
+
+            mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            mySocket.Connect(IPAddress.Parse(IP), port);
+
+            return;
+        }
+    }
+
+    /// <summary>
+    /// Logic Code Here
+    /// </summary>
+    private void Update()
+    {
+        if (socketBehaviour == ESocketBehaviour.Server)
+        {
+            if (potatoesToSpawn > 0)
+            {
+                Vector3 position = new Vector3(UnityEngine.Random.Range(limits.x, limits.y), 4.5f, 0);
+                Instantiate(potatoPrefab, position, Quaternion.identity);
+                potatoesToSpawn--;
+            }
+        }
+
+        if (socketBehaviour == ESocketBehaviour.Client)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                buffer[0] = 1; // Instantiate Message
+                mySocket.Send(buffer, SocketFlags.None);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Closes Client On Application Quit So He Doesnt Spawn Garbage
+    /// </summary>
+    private void OnApplicationQuit()
+    {
+        if (socketBehaviour == ESocketBehaviour.Client)
+        {
+            buffer[0] = 0; // Quit Message
+            mySocket.Send(buffer, SocketFlags.None);
+            mySocket.Close();
+        }
+
+        if (socketBehaviour == ESocketBehaviour.Server)
+        {
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].Close();
+            }
+            mySocket.Close();
+        }
+    }
+
+    public void UpdateSpawnLimits(float x, float y)
+    {
+        limits.x = x;
+        limits.y = y;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                                  Server CallBacks                                                                    //
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    private void AcceptCallback(IAsyncResult asyncResult)
+    {
+        // Get The Socket
+        Socket socket = mySocket.EndAccept(asyncResult);
+        // Add it to the list
+        clients.Add(socket);
+        // Receive info from the socket
+        socket.BeginReceive(buffer, 0, 1024, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+        // Return back to accepting
+        mySocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+    }
+    private void ReceiveCallback(IAsyncResult asyncResult)
+    {
+        // Cache Socket
+        Socket socket = (Socket)asyncResult.AsyncState;
+
+        // Gets Socket
+        try
+        {
+            socket = (Socket)asyncResult;
+        }
+        catch (Exception)
+        {
+
+            // Do Nothing MWAAHHAAHAHAHAHAHHAHAA! Take That EXCEPTIONS!
+        }
+
+        // Gets Bytes Received
+        socket.EndReceive(asyncResult);
+
+        if (buffer[0] == 0)
+        {
+            clients.Remove(socket);
+            socket.Close();
+            return;
+        }
+
+        // IncrementSpawning
+        potatoesToSpawn++;
+
+        // Returns Back To Receiving
+        socket.BeginReceive(buffer, 0, 1024, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+    }
+
+}

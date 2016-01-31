1cec14a6f10fc3527abc987845766bb96abddf04
unknown <1103410@MC10795.uad.ac.uk>
2016-01-31 13:38:49 +0000

    third commit

--- Potato/Assets/Scripts/Client/PotatoManager.cs
+++ Potato/Assets/Scripts/Client/PotatoManager.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class PotatoManager : MonoBehaviour {
 
-    public GameObject potato;
+    public GameObject m_PotatoPrefab;
+    public GameObject m_Potato;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +13,13 @@ public class PotatoManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        if (!m_Potato)
+        {
+            m_Potato = Instantiate<GameObject>(m_PotatoPrefab);
+        }
+
+
+        
 	}
 }
--- Potato/Assets/Scripts/Client/PotatoPreparation.cs
+++ Potato/Assets/Scripts/Client/PotatoPreparation.cs
@@ -11,11 +11,26 @@ public class PotatoPreparation : MonoBehaviour {
 
     private bool m_ReadyToPeel;
     private bool m_ReadyToSend;
+
+    private Animator m_PeelAnimator;
+
+    private bool m_SendingPotato;
+    private float m_TransitionSpeed;
+    public float m_TransitionSpeedMax;
 	// Use this for initialization
 	void Start () {
 
         m_ReadyToPeel = false;
         m_ReadyToSend = false;
+
+        m_SendingPotato = false;
+
+        m_TransitionSpeed = 0.0f;
+
+        m_Socket = GameObject.FindGameObjectWithTag("Client").GetComponent<SocketComm>();
+
+        m_PeelAnimator = m_UnpeeledPotato.GetComponent<Animator>();
+        m_PeelAnimator.speed = 0;
 	}
 	
 	// Update is called once per frame
@@ -29,33 +44,80 @@ public class PotatoPreparation : MonoBehaviour {
             {
                 if (m_UnpeeledPotato)
                 {
-                    m_ReadyToPeel = true;
+                    m_PeelAnimator.speed=1;
+                    //m_ReadyToPeel = true;
+                    if (m_PeelAnimator.GetCurrentAnimatorStateInfo(0).IsName("Peel9") && m_PeelAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9f)
+                    {
+                        m_PeelAnimator.speed = 0;
+                    }
                 }
-                else if (m_PeeledPotato)
+                
+                if (m_PeeledPotato&&!m_UnpeeledPotato)
                 {
                     m_ReadyToSend = true;
                 }
             }
+
+            //m_PeelAnimator.set
+            
              
             if(touches[0].phase == TouchPhase.Ended || touches[0].phase == TouchPhase.Canceled)
             {
-                 if (m_UnpeeledPotato && m_ReadyToPeel)
-                 {
-                    Destroy(m_UnpeeledPotato);
-                    //SendPotato
-                    //m_Socket.RequestPotatoCreation();
-                 }
+                if (m_UnpeeledPotato)
+                {
+                    m_PeelAnimator.speed = 0;
+                }
 
-                 if (m_PeeledPotato && m_ReadyToSend)
-                 {
-                     Destroy(m_PeeledPotato);
-                     //SendPotato
-                     m_Socket.RequestPotatoCreation();
-                     Destroy(gameObject);
-                 }
+                if (m_PeeledPotato && m_ReadyToSend)
+                {
+                    m_Socket.RequestPotatoCreation();
+                    m_SendingPotato = true;
+                }
+
+                if (m_PeelAnimator)
+                {
+
+                    if (m_PeelAnimator.GetCurrentAnimatorStateInfo(0).IsName("Peel9"))
+                    {
+                        if (m_PeelAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9f)
+                        {
+                            Destroy(m_UnpeeledPotato);
+                        }
+                    }
+                }
+            }
+        }
+        else if (m_PeelAnimator)
+        {
+            m_PeelAnimator.speed = 0;
+        }
+
+
+       
+            /*
+        else if (m_UnpeeledPotato)
+        {
+            m_PeelAnimator.speed = 0;
+        }*/
+
+        if (m_SendingPotato)
+        {
+            m_ReadyToSend = false;
+            m_TransitionSpeed += (100.0f * Time.deltaTime);
+            if (m_TransitionSpeed > m_TransitionSpeedMax)
+            {
+                m_TransitionSpeed = m_TransitionSpeedMax;
             }
 
+            transform.position += new Vector3(0.0f, m_TransitionSpeed*Time.deltaTime, 0.0f);
+
+            if (transform.position.y > 20.0f)
+            {
+                m_SendingPotato = false;
+                Destroy(gameObject);
+            }
         }
+        
 
 	}
 }
